39ede89f9f96dcf373eeeb4e4b39e5b6fee9e501
Julia Pangonis <juliapangonis@gmail.com>
2025-07-07 20:29:30 +0000

    done with day 1

---
+++ week-1/ReferencesSolution/References.Tests/AddingNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace References.Tests;
+
+public class AddingNumbers
+{
+    [Fact]
+    public void CanAddTwoNumbers()
+    {
+        // Given
+        int a = 10;
+        int b = 20;
+        int answer;
+
+        // When
+        answer = a + b;
+
+        //Then
+        // Answer should be 30
+        Assert.Equal(30, answer);
+    }
+
+    [Theory]
+    [InlineData(10,20,30)]
+    [InlineData(2,2,4)]
+    public void CanAddAnyTwoIntegers(int a, int b, int expected)
+    {
+        int answer = a + b;
+        Assert.Equal(expected, answer);
+    }
+}

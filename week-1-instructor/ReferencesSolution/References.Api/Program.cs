c6a935d02b480beb88a672c19fbab2a51d11d2b2
Julia Pangonis <juliapangonis@gmail.com>
2025-07-08 20:46:55 +0000

    day 2

--- week-1-instructor/ReferencesSolution/References.Api/Program.cs
+++ week-1-instructor/ReferencesSolution/References.Api/Program.cs
@@ -1,8 +1,18 @@
 
+using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("links")
+    ?? throw new Exception("No Connection String");
+
+
 // Add services to the container. 
+builder.Services.AddMarten(config =>
+{
+    config.Connection(connectionString);
+}).UseLightweightSessions();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -22,3 +32,9 @@ app.UseAuthorization();
 app.MapControllers();  // Reflection the ability to have code that looks at itself.
 
 app.Run();
+
+
+// I will explain this in detail later and you will be bored as heck.
+
+// in .NET 10 (Sept 2025) you won't have to do this any more.
+public partial class Program;
--- week-1/ReferencesSolution/References.Api/Program.cs
+++ week-1/ReferencesSolution/References.Api/Program.cs
@@ -1,33 +1,40 @@
 
-namespace References.Api
+using Marten;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("links")
+    ?? throw new Exception("No Connection String");
+
+
+// Add services to the container. 
+builder.Services.AddMarten(config =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    config.Connection(connectionString);
+}).UseLightweightSessions();
 
-            // Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
-            var app = builder.Build();
+var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseAuthorization();
-
-            //this is where the program finds all the controllers and maps it, during compile time it finds it
-            //it does this by finding the tag, i.e.: [HttpGet("/links")]
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
 }
+
+app.UseAuthorization();
+
+
+app.MapControllers();  // Reflection the ability to have code that looks at itself.
+
+app.Run();
+
+
+// I will explain this in detail later and you will be bored as heck.
+
+// in .NET 10 (Sept 2025) you won't have to do this any more.
+public partial class Program;
